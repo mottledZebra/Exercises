47756d254476398fe995ec350937bc0554c05ad0
Gindill <tolsen@inbox.ru>
2016-10-25 13:48:50 +0300

    первое упражнение

---
+++ Python/The sum of the three numbers.py
@@ -0,0 +1,13 @@
+#Напишите программу, которая считывает три числа и выводит их сумму. 
+#Каждое число записано в отдельной строке. 
+
+#Write a program that reads three numbers and prints their sum. 
+#Each number is written in a separate line.
+
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+
+sum = a + b + c
+print('sum = ' + str(sum))
+
